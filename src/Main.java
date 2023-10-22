dd876777b7285714d7362654651ef7aa8eeb06ce
MariaLarin <mashka2100@mail.ru>
2023-10-22 20:52:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        double num3 = scanner.nextDouble();
+
+        int d = scanner.nextInt();
+
+        switch (d) {
+            case 1 -> System.out.printf("+: %f", num + num3);
+            case 2 -> System.out.printf("-: %.2f", num - num2);
+            case 3 -> System.out.printf("*: %f", num * num3);
+            case 1 -> System.out.printf("/: %f", num / num3);
+            default -> System.out.println("Error");
+        }
+
+        System.out.println();
+
+        if (d == 1) {
+            System.out.printf("+: %d", num + num2);
+        else if (d == 2)
+                System.out.printf("-: %d", num - num2);
+            else if (d == 3)
+                System.out.printf("*: %d", num * num2);
+            else if (d == 4)
+                System.out.printf("/: %d", num / num2);
+        }
+                else{
+            System.out.println("Error");
+        }
+
+        char k = scanner.next().charAt(0);
+
+        if (k == '+'){
+            System.out.printf("+: %d", num+num2);
+        }
+        scanner.close();
+    }
+}
